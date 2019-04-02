474d972bc6b314ce47b2a0149ac35c1748c83850
Rflageolle <rflageol@msudenver.edu>
2019-04-02 15:23:43 -0600

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -64,8 +64,70 @@ public class Parser {
 	   errorCheck ( Name, "NAME" );
 	   Token check = lex.getNextToken();
 	   if (check.isKind("NAME")){
-            	Node first = parseParams();
+		Node first = parseParams();
 		return new Node("params", Name, first, null, null);
 	   }		      
 	   return new Node("params", Name, null, null, null);
    }
+   
+   public Node parseExpr() {
+   	System.out.println("-----> parsing <expr>:);
+	Token check = lex.getNextToken();
+	if ( check.isKind("Number") {
+		return new Node("expression", check, null, null, null)
+	} else {
+		errorCheck(check, "symbol", "(");
+		Node first = parseList();
+		return new Node("expression", first, null, null, null)
+	}
+    }
+    
+    public Node parseList() {
+	System.out.println("-----> parsing <list>:);
+	Token lparen = lex.getNextToken();
+	errorCheck( lparen, "symbol", "(" );
+	
+	Token check = lex.getNextToken();
+	
+	if ( check.isSymbol(")") ) {
+		return new Node("list", null, null, null);	
+	}
+   	else {
+		lex.putBackToken(check);
+		Node first = parseItems();
+		return new Node("list", first, null, null);
+	}
+    }
+			   
+   public Node parseItems() {
+	System.out.println("-----> parsing <list>:);
+   	Node first = parseExpression();
+   	Node second = null;
+   	Token check = lex.getNextToken();
+	if ( check.isKind("Number") ) {
+		second = parseExpression();
+	}
+	return new Node("items", first, second, null);
+   }
+	   
+  // check whether token is correct kind
+  private void errorCheck( Token token, String kind ) {
+    if( ! token.isKind( kind ) ) {
+      System.out.println("Error:  expected " + token +
+                         " to be of kind " + kind );
+      System.exit(1);
+    }
+  }
+
+  // check whether token is correct kind and details
+  private void errorCheck( Token token, String kind, String details ) {
+    if( ! token.isKind( kind ) ||
+        ! token.getDetails().equals( details ) ) {
+      System.out.println("Error:  expected " + token +
+                          " to be kind= " + kind +
+                          " and details= " + details );
+      System.exit(1);
+    }
+  }
+
+}
